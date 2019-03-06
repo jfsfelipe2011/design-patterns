2e3e02210d872927f9534d785f8ce218e80cb97b
José Felipe <jose.felipe@kinghost.com.br>
2019-03-06 20:04:18 -0300

    Subindo index

--- Behavioral/Observer/index.php
+++ Behavioral/Observer/index.php
@@ -2,7 +2,24 @@
 
 declare(strict_types=1);
 
+use App\User;
+use App\UserObserver;
+
 require_once __DIR__ . '/vendor/autoload.php';
 
-echo "================= Alteração de Status =================";
+echo "================= Usuários alterados =================";
 echo "<br><br><br>";
+
+$user = new User();
+$observer = new UserObserver();
+
+$user->attach($observer);
+
+$user->trocaEmail("juan@gmail.com");
+$user->trocaEmail("saulo@gmail.com");
+$user->trocaEmail("jaqueline@gmail.com");
+
+foreach ($observer->getUsuariosAlterados() as $usuario) {
+    var_dump($usuario);
+    echo "<br><br>";
+}
